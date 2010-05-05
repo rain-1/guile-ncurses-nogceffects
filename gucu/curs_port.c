9113da6982cf23cd9f9b50f4c4abfd41e920a133
Michael Gran <spk121@yahoo.com>
2010-05-04 19:30:14 -0700

    Improve wide character support of curses ports
    * gucu/curs_port.c (port_read): read bytes and build wide characters

--- gucu/curs_port.c
+++ gucu/curs_port.c
@@ -29,16 +29,24 @@ port_read (void *cookie, char *buf, size_t siz)
 {
   SCM port = PTR2SCM(cookie);
 
-#if 0
-  size_t bytes_read;
+#ifdef GUILE_CHARS_ARE_UCS4
+  int c;
+  if (siz >= 1)
+    {
+      c = scm_get_byte_or_eof (port);
 
-  if (siz > SSIZE_MAX)
-    bytes_read = scm_c_read (port, buf, SSIZE_MAX);
+      if (c == EOF)
+	return 0;
+      else
+	buf[0] = c;
+
+      return 1;
+    }
   else
-    bytes_read = scm_c_read (port, buf, siz);
-
-  return bytes_read;
+    return PORT_ERR;
 #else
+  /* For Guile 1.8.x, we use scm_read_char so we can preserve line
+     and column information.  */
   SCM c;
   if (siz >= 1)
     {
@@ -62,7 +70,7 @@ port_write (void *cookie, const char *buf, size_t siz)
   size_t i;
   SCM port = PTR2SCM(cookie);
 
-#if 0
+#ifdef GUILE_CHARS_ARE_UCS4
   if (siz > SSIZE_MAX)
     {
       scm_c_write (port, buf, SSIZE_MAX);
@@ -120,10 +128,8 @@ gucu_newterm (SCM type, SCM outp, SCM inp)
 	      "newterm");
   SCM_ASSERT (scm_is_true (scm_input_port_p (inp)), inp, SCM_ARG3, "newterm");
 
-  /* Convert the input port to a new stream */
-  SCM infd = scm_fileno (inp);
-  int c_infd = scm_to_int (infd);
-  c_inp = fdopen (c_infd, "rb");
+  /* Convert the input port to a special stream */
+  c_inp = fopencookie (SCM2PTR(inp), "rb", port_funcs);
   if (c_inp == NULL)
     scm_syserror ("newterm");
 
@@ -159,10 +165,10 @@ gucu_getwin (SCM port)
   WINDOW *c_win;
   SCM s_c;
   unsigned char c;
-  
+
   SCM_ASSERT (scm_is_true (scm_input_port_p (port)), port, SCM_ARG1, "getwin");
 
-  //ifdef HAVE_FOPENCOOKIE  
+  //ifdef HAVE_FOPENCOOKIE
 #if 0
   fp = fopencookie (&port, "rb", port_funcs);
 
@@ -193,7 +199,7 @@ gucu_getwin (SCM port)
 
   /* Create a window fromt the contents of the port */
   c_win = getwin (fp);
-  fclose (fp);      
+  fclose (fp);
 #endif
 
   if (c_win == NULL)
@@ -210,9 +216,9 @@ gucu_putwin (SCM win, SCM port)
   WINDOW *c_win;
   FILE *fp;
   int ret;
-  
+
   SCM_ASSERT (_scm_is_window (win), win, SCM_ARG1, "putwin");
-  SCM_ASSERT (scm_is_true (scm_output_port_p (port)), port, SCM_ARG2, 
+  SCM_ASSERT (scm_is_true (scm_output_port_p (port)), port, SCM_ARG2,
 	      "putwin");
 
   c_win = _scm_to_window (win);
@@ -222,20 +228,20 @@ gucu_putwin (SCM win, SCM port)
   {
     char *debug_str;
     size_t debug_len;
-    
+
     fp = fopencookie (&port, "wb", port_funcs);
-    
+
     if (fp == NULL)
       return SCM_BOOL_F;
-    
+
     ret = putwin (c_win, fp);
-    
+
     if (ret == ERR)
       return SCM_BOOL_F;
-    
+
     debug_str = scm_to_locale_stringn (scm_get_output_string (port), &debug_len);
     scm_display (scm_from_size_t (debug_len), scm_current_output_port ());
-    
+
     /* The string is not closed here, so that its contents can be read */
     /* fclose (fp); */
   }
